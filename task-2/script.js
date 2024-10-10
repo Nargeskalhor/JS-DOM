b67414c97dc68e0a38b2122b57dd520028fc425e
narges <13.narges.kalhor.82@gmail.com>
2024-10-10 12:01:17 +0330

    task-2

--- task-2/script.js
+++ task-2/script.js
@@ -1,2 +1,14 @@
 "use strict";
-document.querySelector(".text");
+document.getElementById("button").addEventListener("click", function () {
+  const input = document.getElementById("input").value;
+  const pEl = document.getElementById("pEl");
+
+  if (input === "") {
+    pEl.style.color = "red";
+    pEl.textContent = "please enter a value";
+  } else {
+    pEl.style.color = "blue";
+
+    pEl.textContent = input;
+  }
+});
